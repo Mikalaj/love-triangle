9f246c9771517fa14d8c10e2150bb82f58950afa
Fedytnik, Nikolay <n.fedytnik@itransition.com>
2019-02-26 10:19:55 +0300

    Implement function for searching number of love triangles

--- src/index.js
+++ src/index.js
@@ -3,5 +3,28 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+    let triangles = 0;
+    preferences = preferences.map(function (val) {
+        return val - 1;
+    });
+    for (let i = 0; i < preferences.length - 2; i++) {
+        if (preferences[i] !== null) {
+            if (preferences[i] === i) {
+                preferences[i] = null;
+                continue;
+            }
+            if ((preferences[preferences[preferences[i]]] === i)
+            ) {
+                preferences[preferences[preferences[i]]] = null;
+                preferences[preferences[i]] = null;
+                preferences[i] = null;
+                triangles++;
+                continue;
+            }
+            preferences[i] = null;
+        }
+    }
+
+    return triangles;
+}
+;
